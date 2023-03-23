1fd837723d93fc8f4b53063eb7257690a26c2555
abhishekpanditt <badi.abhi@gmail.com>
2023-03-24 00:21:16 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,18 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-
+         /*
+        try {
+            CurrentAccount ca1 = new CurrentAccount("Rocky", 5500, "ABBC");
+            SavingsAccount sa1 = new SavingsAccount("Rockysa1",60000,100000,2);
+            System.out.println(sa1.getBalance());
+            System.out.println(sa1.getSimpleInterest(3));
+            System.out.println(sa1.getCompoundInterest(2,3));
+            System.out.println(sa1.generateAccountNumber(5,1));
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        */
     }
 }
